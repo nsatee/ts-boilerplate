047834a5cf7a605475b43b854106ed403d5a384c
Nattawod Satee <nattawodsatee@Nattawods-MacBook-Pro.local>
2020-11-29 00:12:48 -0800

    Add lint

--- src/declaration.d.ts
+++ src/declaration.d.ts
@@ -1,6 +1,6 @@
 declare module "*.svg";
-declare module '*.jpeg';
-declare module '*.jpg';
-declare module '*.jpeg';
-declare module '*.png';
+declare module "*.jpeg";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.png";
 
